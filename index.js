6a045ee6cd2851a09b7be2c50f1f902c5d5f8610
Pijey06 <jamyamber.spict@gmail.com>
2024-03-20 11:30:58 +0800

    Fifth commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 console.log("SUP COMRADE!!!!");
 //generateNumber(75);
 
-let isB = false,
+let 
+isB = false,
 isI = false,
 isN = false,
 isG = false,
 isO = false;
+
+let
+BNumber, 
+INumber, 
+NNumber, 
+GNumber, 
+ONumber;
 
 function generateNumber(max){
     //add = +
@@ -19,27 +27,55 @@ function generateNumber(max){
     //if... else if ... else 
     // GT >, GTE > =, LT <, LTE < =, EQ =, NEQ 
     if (randomNumber <= 15){
-        isB = true;
-        console.log(`The number is B.`);
+       BNumber = randomNumber;
+        isB = true; 
+        console.log(`The number ${randomNumber} is B.`);
     } else if (randomNumber >= 16 && randomNumber <= 30) {
-        isI = true;
-        console.log(`The number is I.`);
+        INumber = randomNumber;
+        isI = true; 
+        console.log(`The number ${randomNumber} is I.`);
     } else if (randomNumber >= 31 && randomNumber <= 45) {
-        isN = true;
-        console.log(`The number is N.`);
+       NNumber = randomNumber;
+        isN = true; 
+        console.log(`The number ${randomNumber} is N.`);
     } else if (randomNumber >=46 && randomNumber <= 60) {
-        isG = true;
-        console.log(`The number is G.`);
+       GNumber = randomNumber;
+        isG = true; 
+        console.log(`The number ${randomNumber} is G.`);
     } else if (randomNumber >=61 && randomNumber <= 75) {
-        isO = true;
-        console.log(`The number is O.`);
+       ONumber = randomNumber;
+        isO = true; 
+        console.log(`The number ${randomNumber} is O.`);
     } 
     else { 
         console.log(`The number is invalid`);
         }
     if (isB == true && isI == true && isN == true && isG == true && isO == true) {
             console.log('Comrade you have a bingo!!!!!!!');
+            console.table({
+                B: BNumber,
+                I: INumber,
+                N: NNumber,
+                G: GNumber,
+                O: ONumber,
+            })
         }
     return console.log(randomNumber);
 }
 let counter = 10;
+
+
+//while (counter > 0){
+   // generateNumber(75);
+   // counter -= 1;
+//}
+
+while ( 
+    isB == false || 
+    isI == false || 
+    isN == false || 
+    isG == false || 
+    isO == false 
+    ) {
+        generateNumber(75);
+}
